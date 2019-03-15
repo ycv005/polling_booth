01aa86b5c7f7bd17afe00e70dae249c92a496f08
ycv005 <ycverma005@gmail.com>
2019-03-15 22:34:00 +0530

    static files

--- polling_booth/admin.py
+++ polling_booth/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Question,Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+#this way you can customize how the Question admin form
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None,               {'fields': ['ques_text']}),
+    #     ('Date information', {'fields': ['publish_date']}),
+    # ]
+    list_display = ('ques_text', 'publish_date')
+    list_filter = ['publish_date']
+    search_fields = ['ques_text']
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question,QuestionAdmin)
